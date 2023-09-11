4a3564cd0cdf57b6fadac57917ec5e06d5f11400
VishalP-SoluteLabs <125434786+VishalP-SoluteLabs@users.noreply.github.com>
2023-09-11 18:02:08 +0530

    Add files via upload

---
+++ Techs/Testing/FrontEnd/SocialMediaRestWebApp/build/precache-manifest.a406d76a3f6c29e6f4b914a913374646.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "860f7c38206b06ef1619",
+    "url": "/static/css/main.c846ed50.chunk.css"
+  },
+  {
+    "revision": "860f7c38206b06ef1619",
+    "url": "/static/js/main.860f7c38.chunk.js"
+  },
+  {
+    "revision": "2bf4a5b8f443f997c80e",
+    "url": "/static/js/1.2bf4a5b8.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "88902663f644a96972239d517d13ccd6",
+    "url": "/index.html"
+  }
+];
