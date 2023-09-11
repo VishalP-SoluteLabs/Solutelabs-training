54f0a1c5e156775ee2e1864a7641dc13ff037838
VishalP-SoluteLabs <125434786+VishalP-SoluteLabs@users.noreply.github.com>
2023-09-11 17:50:35 +0530

    Add files via upload

---
+++ Techs/ReactJs/Projects/first-react-app/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const App = () => {
+  return (
+      <>
+    <MyName />
+    <div>App {3+3} = 3+3</div>
+    <p>Hello</p> 
+  </>  
+  )
+}
+
+
+const MyName = () => {
+    return (
+        <h1>Vishal Pansuriya</h1>
+    )
+}
+
+export default App
